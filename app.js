57f9fd6dbb95537126da5ef532cd052364309259
asadmunirdev <asadmunir6645@gmail.com>
2024-06-04 23:23:09 -0700

    adding milliseconds in time

--- app.js
+++ app.js
@@ -6,8 +6,20 @@ let intervalId;
 // start button
 const startTimer = () => {
   intervalId = setInterval(() => {
-    countDownElement.innerText = startCount++;
-  }, 1000);
+    const minutes = Math.floor(startCount / 6000);
+    const seconds = Math.floor((startCount % 6000) / 100);
+    const milliseconds = startCount % 100;
+
+    countDownElement.innerHTML = `<span style="color: #FFFF00;">${minutes
+      .toString()
+      .padStart(2, "0")}</span>:<span style="color: #FFFF00;">${seconds
+      .toString()
+      .padStart(2, "0")}</span>:<span style="color: #FFFF00;">${milliseconds
+      .toString()
+      .padStart(2, "0")}</span>`;
+
+    startCount++;
+  }, 10); // 10 milliseconds for accurate milliseconds display
 };
 
 // stop button
@@ -19,18 +31,29 @@ const stopTimer = () => {
 // reset button
 const resetTimer = () => {
   startCount = 0;
-  countDownElement.innerText = startCount;
+  countDownElement.innerHTML =
+    '<span style="color: #FFFF00;">00</span>:<span style="color: #FFFF00;">00</span>:<span style="color: #FFFF00;">00</span>';
   clearInterval(intervalId);
 };
 
-// time button
+// showtime button
 const showStopValue = () => {
+  const minutes = Math.floor((startCount - 1) / 6000);
+  const seconds = Math.floor(((startCount - 1) % 6000) / 100);
+  const milliseconds = (startCount - 1) % 100;
+
   const newPara = document.createElement("p");
-  newPara.innerText = `the stop time is ${startCount - 1}`;
+  newPara.innerHTML = `The stop time is <span style="color: #FFFF00;">${minutes
+    .toString()
+    .padStart(2, "0")}</span>:<span style="color: #FFFF00;">${seconds
+    .toString()
+    .padStart(2, "0")}</span>:<span style="color: #FFFF00;">${milliseconds
+    .toString()
+    .padStart(2, "0")}</span>`;
   resetValueElement.append(newPara);
 };
 
-// time button
+// cleartime button
 const clearTimeValue = () => {
   resetValueElement.innerHTML = "";
 };
